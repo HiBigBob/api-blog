9a3435886501c2c36ae16608ce1c9ca991c3f43f
Adrien Bouttier <hibigbob@gmail.com>
2015-09-22 13:43:24 +0200

    Delete useless

--- app.js
+++ app.js
@@ -14,13 +14,6 @@ var posts = require('./routes/posts');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-
 app.set('jwtTokenSecret', 'YOUR_SECRET_STRING');
 mongoose.connect(config.database);
 
@@ -33,12 +26,6 @@ db.once('open', function (callback) {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
