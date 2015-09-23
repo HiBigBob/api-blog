8da53f275625767b4ab9a8863703e7ddfee6699c
Adrien Bouttier <hibigbob@gmail.com>
2015-09-23 21:17:22 +0200

    Add models

---
+++ models/post.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('Post', new Schema({
+    userId: String,
+    title: String,
+    content: String,
+    createTime: { type: Date, default: Date.now }
+}));
