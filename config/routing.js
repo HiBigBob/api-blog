8a7e0a6dab780cced312fbc6a39e942ab4d66a34
Adrien Bouttier <hibigbob@gmail.com>
2015-11-14 14:03:46 +0100

    fix typo

--- config/routing.js
+++ config/routing.js
@@ -1,14 +1,14 @@
-var user 		= require('../controllers/user');
-var setup 		= require('../controllers/setup');
-var auth 		= require('../controllers/auth');
-var post 		= require('../controllers/post');
-var tag 		= require('../controllers/tag');
+var user 			= require('../controllers/user');
+var setup 			= require('../controllers/setup');
+var authenticate 	= require('../controllers/auth');
+var post 			= require('../controllers/post');
+var tag 			= require('../controllers/tag');
 
-var auth 		= require('../middlewares/auth');
-var require 	= require('../middlewares/require');
+var auth 			= require('../middlewares/auth');
+var require 		= require('../middlewares/require');
 
 module.exports.set = function(app) {
-	app.use('/authenticate', auth);
+	app.use('/authenticate', authenticate);
 	app.use('/setup', setup);
   	app.use('/users', [auth, require], user);
   	app.use('/posts', post);
